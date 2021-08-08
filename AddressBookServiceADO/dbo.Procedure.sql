8d9e913a8cb0cbb3e695bbfd203cdd189a8f231f
sahithipaladi <86294777+sahithipaladi@users.noreply.github.com>
2021-08-08 20:01:24 +0530

    Update dbo.Procedure.sql

--- AddressBookServiceADO/dbo.Procedure.sql
+++ AddressBookServiceADO/dbo.Procedure.sql
@@ -15,6 +15,6 @@ AS
 BEGIN
 	Insert into AddressBookServiceTable values(@FirstName,@LastName,@Address,@City,@State,@Zip,@PhoneNumber,@Email,@AddressBookName,@RelationType)
 	Insert into AddressBook(AddressBookName) values(@AddressBookName)
-	
+	Insert into Contact_Person(FirstName,LastName,Address,City,State,zip,PhoneNumber,Email,AddedDate) values(@FirstName,@LastName,@Address,@City,@State,@Zip,@PhoneNumber,@Email,GETDATE())
 	Insert into ContactType(ContactTypeName) values(@RelationType)
 END
